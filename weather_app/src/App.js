76bb6fefe33ead04433aed970fc15ead00f1a697
Nimoo2003 <ucse21044@stu.xim.edu.in>
2023-07-18 20:29:52 +0530

    UPDATED APP.js

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -11,9 +11,15 @@ function App() {
           </div>
           <div className="section section__temperature">
             <div className="icon">
-              <h3></h3>
+              <h3>London, GB</h3>
+              <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="weatherIcon" />
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1>34 C</h1>
             </div>
           </div>
+          { /* BOTTOM DESCRIPTION */}
         </div>
       </div>
     </div>
