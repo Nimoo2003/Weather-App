d6c5c520da4847687e042cef5d3aee1b7f9d44b2
Nimoo2003 <ucse21044@stu.xim.edu.in>
2023-07-18 20:17:19 +0530

    created div tags and added classnames to it and created a button

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,9 +1,21 @@
 import hotBg from "./assets/sunny.jpg"
-import coldBg from "./assets/cold/jpg"
+import coldBg from "./assets/cold.jpg"
 function App() {
   return (
-    <div className="app" style={{backgroundImage:`url($(coldBg))`}}>
-      <h1>Hello</h1>
+    <div className="app" style={{backgroundImage: `url(${coldBg})`}}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section_inputs">
+            <input type="text" name="city" placeholder="Enter city..." />
+            <button>F</button>
+          </div>
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3></h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
