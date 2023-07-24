bdc614329d6fb104b78da23c0ca20d428113bc32
Nimoo2003 <ucse21044@stu.xim.edu.in>
2023-07-24 22:25:01 +0530

    changed a bit

--- weather_app/src/weatherService.js
+++ weather_app/src/weatherService.js
@@ -1,34 +1,19 @@
-require('dotenv-safe').config();
+require ('dotenv').config();
 
-const fetch = require('node-fetch');
+const API_KEY = process.env.API_KEY
 
+const getFormattedWeather = async(city , units='metric') => {
 
-const API_KEY = process.env.API_KEY;
+    const URL = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}';
 
-const getFormattedWeather = async (city, units = 'metric') => {
-  
-  
-    const baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}'; // Replace this with the actual API URL
-    const URL = `${baseURL}?q=${city}&units=${units}&appid=${API_KEY}`;
+    const data = await fetch(URL) 
+        .then((res) => res.json())
+        .then((data) => data);
 
-  try {
-    
-    const response = await fetch(URL);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    const data = await response.json();
-    console.log(data);
-    return data; 
-  } catch (error) {
-    console.error('Error fetching weather data:', error);
-    return null;
-  }
+        console.log(data);
 };
 
-module.exports = { getFormattedWeather };
+export {getFormattedWeather};
 
-// const URL = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}';
 
 
