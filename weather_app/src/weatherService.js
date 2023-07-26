119066d426d73d6f876d6d9f6ea670762c81fcad
Nimoo2003 <ucse21044@stu.xim.edu.in>
2023-07-26 21:04:10 +0530

    added in weatherjs

--- weather_app/src/weatherService.js
+++ weather_app/src/weatherService.js
@@ -1,18 +1,26 @@
 const API_KEY = "5d61b87ea6f4bdecb91aa3db05a60cea";
 
-const getFormattedWeather = async (city, units = 'metric') => {
+const makeIconURL = (iconId) => "https://openweathermap.org/img/wn/02d${iconId}@2x.png";
+
+const getFormattedWeather = async (city, units = "metric") => {
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
 
   const data = await fetch(URL)
     .then((res) => res.json())
     .then((data) => data);
 
-  const { weather, main: { temp, feels_like, temp_min, temp_max, pressure, humidity }, wind: { speed }, sys: { country }, name } = data;
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = data;
   const { description, icon } = weather[0];
 
   return {
     description,
-    icon,
+    iconURL : makeIconURL(icon),
     temp,
     feels_like,
     temp_min,
@@ -26,7 +34,3 @@ const getFormattedWeather = async (city, units = 'metric') => {
 };
 
 export { getFormattedWeather };
-
-
-
-
