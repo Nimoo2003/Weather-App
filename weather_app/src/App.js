22c15023553350f08067073743df645341c771a7
Nimoo2003 <ucse21044@stu.xim.edu.in>
2023-07-27 21:54:40 +0530

    updated app.js

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -4,10 +4,14 @@ import Description from "./components/Description";
 import React, { useEffect, useState } from 'react';
 import { getFormattedWeather } from "./weatherService";
 function App() {
+
+  const [weather , setWeather] = useState(null) ;
+
   useEffect(() => {
     const fetchWeatherData = async () => {
       const data = await getFormattedWeather("paris");
-      console.log(data);
+      setWeather(data);
+     
     };
     fetchWeatherData();
   },[]);
@@ -15,14 +19,16 @@ function App() {
   return (
     <div className="app" style={{backgroundImage: `url(${coldBg})`}}>
       <div className="overlay">
-        <div className="container">
+        {
+          weather && (
+            <div className="container">
           <div className="section section__inputs">
             <input type="text" name="city" placeholder="Enter city..." />
             <button>F</button>
           </div>
           <div className="section section__temperature">
             <div className="icon">
-              <h3>London, GB</h3>
+              <h3>{`${weather.name} , ${weather.country}`}</h3>
               <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="weatherIcon" />
               <h3>Cloudy</h3>
             </div>
@@ -33,6 +39,9 @@ function App() {
           { /* BOTTOM DESCRIPTION */}
           <Description />
         </div>
+
+        )} 
+        
       </div>
     </div>
   );
